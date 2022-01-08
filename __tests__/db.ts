b8d4764ee0bd1004a5708a2fc2c3fe921a4afcdc
Tim de Jager <tdejager@gmail.com>
2022-01-08 10:08:41 +0100

    feat: updated prisma to be able to run against seeded db

--- __tests__/db.ts
+++ __tests__/db.ts
@@ -1,9 +1,10 @@
 import {PrismaClient} from "@prisma/client";
 
-describe("Does it work?", () => {
-    it("Works?", async () => {
+describe("Database", () => {
+    it("Is database seeded", async () => {
         const prisma = new PrismaClient();
         await prisma.$connect();
-        const result = await prisma.recipe.count()
+        const result = await prisma.recipe.count();
+        expect(result).toBe(2);
     });
 });
