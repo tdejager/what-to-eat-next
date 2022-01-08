192f79f1b13ae8511726ff4fd9729ccef12ace8f
Tim de Jager <tdejager89@gmail.com>
2022-01-08 12:41:07 +0100

    wip: working on prisma integration

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,18 @@
-import type {NextPage} from 'next'
+import type { GetServerSideProps, InferGetServerSidePropsType, NextPage } from 'next'
 import Image from 'next/image'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import { PrismaClient, Recipe } from '@prisma/client'
 
 type RecipeProps = {
     text: string,
-    url?: string,
+    url: string | null,
+}
+
+const getRecipes = async () => {
+    const prisma = new PrismaClient();
+    const results = await prisma.recipe.findMany({ take: 10 });
+    return results.map(function(r) { return { text: r.title, url: r.url } });
 }
 
 /**
@@ -13,11 +20,11 @@ type RecipeProps = {
  * @param param
  * @returns
  */
-const Recipe = ({text, url}: RecipeProps) => {
+const Recipe = ({ text, url }: RecipeProps) => {
     const urlRendered = url ? <a className={"text-center underline"} href={url}>Click for recipe</a> : null;
     return <div
         className='border-gray-700 border flex flex-col justify-items-center gap-y-3 rounded-xl p-5 bg-gradient-to-b from-gray-100 to-transparent'>
-        <Image src="/pie.jpg" width={200} height={200} alt={"Picture of the recipe"}/>
+        <Image src="/pie.jpg" width={200} height={200} alt={"Picture of the recipe"} />
         <p className='text-xl text-center'>{text}</p>
         {urlRendered}
     </div>
@@ -26,24 +33,31 @@ const Recipe = ({text, url}: RecipeProps) => {
 /**
  * List of number of recipes
  */
-const RecipeListing = () => {
-
+const RecipeListing: React.FC<{ recipeList: Recipe[] }> = (props) => {
+    const recipes = props.recipeList.map((r) => {
+        <Recipe url={r.url} text={r.title} />
+    });
     return (
-        <div className='flex flex-col items-center gap-y-1'>
-            <Recipe text="First recipe"/>
-            <Recipe text="Recipe 2" url={"https://www.culy.nl/recepten/vegan-bibimbap/"}/>
+        <div className='flex flex-col items-center gap-y-1 h-full'>
+            <Recipe text="First recipe" />
+            <Recipe text="Recipe 2" url={"https://www.culy.nl/recepten/vegan-bibimbap/"} />
         </div>
     )
 }
 
 /// The recipe page
-const RecipeListPage: NextPage = () => {
+const RecipeListPage = (recipes: InferGetServerSidePropsType<typeof getServerSideProps>) => {
     return (
-        <div className='flex flex-col items-center'>
-            <h1 className="text-6xl font-normal leading-normal mt-0 mb-5">What to eat next?</h1>
-            <RecipeListing/>
+        <div className='flex flex-col items-center h-screen w-screen'>
+            <h1 className="text-xl md:text-3xl lg:text-6xl font-normal leading-normal mt-0 mb-5">What to eat next?</h1>
+            <RecipeListing recipeList={recipes.recipes} />
         </div>
     )
 }
 
 export default RecipeListPage
+
+export const getServerSideProps: GetServerSideProps<{ recipes: Recipe[] }> = async (ctx) => {
+    const recipes = await getRecipes();
+    return { props: { recipes: recipes } };
+};
